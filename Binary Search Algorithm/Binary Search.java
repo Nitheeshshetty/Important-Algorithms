db78da530f745a9d8a64a9a802913fcfbca82b47
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-19 15:57:25 +0530

    Create Binary Search.java

---
+++ Binary Search Algorithm/Binary Search.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+
+
+//The actual algorithm is written in this class
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        int start=0,end=n-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==k)
+            {
+               return mid;
+            }
+            else if(arr[mid]>k){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
