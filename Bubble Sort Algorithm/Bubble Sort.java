45422a8a3a606e92d40628d9eb83ff1435bc474a
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-21 17:37:33 +0530

    Update Bubble Sort.java

--- Bubble Sort Algorithm/Bubble Sort.java
+++ Bubble Sort Algorithm/Bubble Sort.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 class Solution
 {
-	public static void bubbleSort(int arr[], int n)
+    public static void bubbleSort(int arr[], int n)
     {
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
@@ -17,7 +17,6 @@ class Solution
     }
 }
 
-//{ Driver Code Starts.
 
 class Sorting
 {
@@ -29,36 +28,28 @@ class Sorting
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-	public static void main(String args[])
+    public static void main(String args[])
+    {
+	//taking input using Scanner class
+	Scanner sc = new Scanner(System.in);	
+	//taking total testcases
+	int t = sc.nextInt();
+	while(t>0)
 	{
-	    //taking input using Scanner class
-		Scanner sc = new Scanner(System.in);
-		
-		//taking total testcases
-		int t = sc.nextInt();
-		while(t>0)
-		{
-		    //taking total elements
-			int n = sc.nextInt();
-			
-			//creating a new array of length n
-			int arr[] = new int[n];
-			
-			//inserting elements to the array
-			for(int i=0;i<n;i++)
-			{
-				arr[i] = sc.nextInt(); 
-			}
-			
-			
-			//calling bubbleSort() method
-			new Solution().bubbleSort(arr,n);
-			
-			//calling printArray() method
-			printArray(arr);
-			
-		t--;	
-		}
+	    //taking total elements
+	    int n = sc.nextInt();	
+	    //creating a new array of length n
+	    int arr[] = new int[n];	
+	    //inserting elements to the array
+	    for(int i=0;i<n;i++)
+	    {
+		arr[i] = sc.nextInt(); 
+	    }	
+	    //calling bubbleSort() method
+	    new Solution().bubbleSort(arr,n);	
+	    //calling printArray() method
+	    printArray(arr);	
+	    t--;	
 	}
-
+    }
 }
