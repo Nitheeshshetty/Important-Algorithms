ffa4f281b2f5b8685d2661bb1ca717ceda995d97
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-23 20:54:29 +0530

    Update dfsOfGraph.java

--- DFS Algorithm/dfsOfGraph.java
+++ DFS Algorithm/dfsOfGraph.java
@@ -3,15 +3,13 @@ import java.lang.*;
 import java.io.*;
 class GFG {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String[] s = br.readLine().trim().split(" ");
             int V = Integer.parseInt(s[0]);
             int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj =
-                new ArrayList<ArrayList<Integer>>();
+            ArrayList<ArrayList<Integer>> adj =new ArrayList<ArrayList<Integer>>();
             for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
             for (int i = 0; i < E; i++) {
                 String[] S = br.readLine().trim().split(" ");
@@ -32,7 +30,6 @@ class GFG {
 
 
 class Solution {
-    // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean visited[]=new boolean[V];
         ArrayList<Integer>ans=new ArrayList<Integer>();
