859ad4ff576a5e768273bb5c20563d31c42dc1fc
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-24 17:59:19 +0530

    Update deleteAlternate.java

--- Delete Alternate Nodes/deleteAlternate.java
+++ Delete Alternate Nodes/deleteAlternate.java
@@ -1,6 +1,3 @@
-//{ Driver Code Starts
-/*package whatever //do not write package name here */
-
 import java.io.*;
 import java.util.*;
 
@@ -14,36 +11,30 @@ class Node {
 }
 
 class GFG {
-    	
-
-    
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t-- > 0){
-		    
-		    Node head1 = null,tail1 = null;
-		    
-		    int n1 = sc.nextInt();
+    public static void main (String[] args) {
+     	Scanner sc = new Scanner(System.in);
+	int t = sc.nextInt();
+	while(t-- > 0){	    
+	    Node head1 = null,tail1 = null;
+	    int n1 = sc.nextInt();
             int d1 = sc.nextInt();		    
-		    head1 = new Node(d1);
-		    tail1 = head1;
-		    
-		    while(n1-- > 1){
-		        tail1.next = new Node(sc.nextInt());
-		        tail1 = tail1.next;
-		    }
-		    
-		    Solution obj = new Solution();
-		    obj.deleteAlternate(head1);
-		    while(head1 != null){
-		        System.out.print(head1.data + " ");
-		        head1 = head1.next;
-		    }
-		    System.out.println();
-		}
+ 	    head1 = new Node(d1);
+	    tail1 = head1;    
+	    while(n1-- > 1)
+	    {
+		tail1.next = new Node(sc.nextInt());
+		tail1 = tail1.next;
+	    }    
+	    Solution obj = new Solution();
+	    obj.deleteAlternate(head1);
+	    while(head1 != null)
+	    {
+		System.out.print(head1.data + " ");
+		head1 = head1.next;
+	    }
+	    System.out.println();
 	}
-
+    }
 }
 
 
