527e362bc27a727baf274ee911087aa5d6ad31ed
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-02 18:20:47 +0530

    Update shortest_distance.java

--- Floyd Warshall Algorithm/shortest_distance.java
+++ Floyd Warshall Algorithm/shortest_distance.java
@@ -39,12 +39,12 @@ class Solution
         int n=mat.length;
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
+                for(int j=0;j<n;j++)
+                {
                     if(mat[i][k]==-1 || mat[k][j]==-1)
                         continue;
-                    if(mat[i][j]==-1 || mat[i][k]+mat[k][j]<mat[i][j]){
+                    if(mat[i][j]==-1 || mat[i][k]+mat[k][j]<mat[i][j])
                         mat[i][j]=mat[i][k]+mat[k][j];
-                    }
                 }
             }
         }
